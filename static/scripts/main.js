cc7e761b81f390954498b75a7f999be6c9ccf49d
Elton Kok <elton.kok@mail.utoronto.ca>
2019-05-07 15:03:14 -0400

    Add working copy of website

---
+++ static/scripts/main.js
@@ -0,0 +1,24 @@
+$(document).ready( function() {
+    $('navbar').show();
+})
+$(window).scroll(function(e) {
+    // add/remove class to navbar when scrolling to hide/show
+    // add/remove active class on the nav-links to show where user is on the web page
+    var scroll = $(window).scrollTop();
+    if (scroll < 100) {
+        $('.navbar').addClass("navbar-hide");
+        $('nav a.active').removeClass('active');
+        $('nav a:first').addClass('active');
+    } else {
+        $('.navbar').removeClass("navbar-hide");
+        $('.wrapper').each(function(i) {
+            if (this['id'] == 'contactMe' && $(this).position().top <= scroll+400) {
+                $('nav a.active').removeClass('active');
+                $('nav a').eq(i).addClass('active');
+            } else if ($(this).position().top <= scroll + 70) {
+                $('nav a.active').removeClass('active');
+                $('nav a').eq(i).addClass('active');
+            }
+        })
+    }
+});
